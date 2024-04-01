c39c9f51ea9156bdf6edf71bfec4add232e2df23
Gustavo Guimarães <49100132+gustavo234guimaraes@users.noreply.github.com>
2024-04-01 16:29:56 -0300

    Add files via upload

--- R/convert_xml.R
+++ R/convert_xml.R
@@ -1,14 +1,15 @@
 convert_xml<-function(file,ext='.csv',out_dir=""){
   
-  arqXML<-xmlParse(file)
-  node<-names(xmlRoot(arqXML))
+  arqXML<-read_xml(file)
+  node<-xml_name(xml_child(arqXML))
   
-  
-  bind_rows(xpathApply(arqXML, paste0("//",node), function(x) {
-    parent <- data.frame(as.list(xmlAttrs(x)), stringsAsFactors = FALSE)
-    kids <- bind_rows(lapply(xmlChildren(x), get_children))
+  nodes<-xml_find_all(arqXML, paste0("//", node))
+  lapply(nodes, function(x){
+    parent <- data.frame(as.list(xml_attrs(x)), stringsAsFactors = FALSE)
+    kids <- bind_rows(lapply(xml_children(x), get_children))
     cbind.data.frame(parent, kids, stringsAsFactors = FALSE)
-  })) %>% suppressMessages() %>% 
+  }) %>% unlist(recursive = F) %>%
+    suppressMessages() %>% 
     write_out(ext=ext,file=file,path=out_dir)
   
   return(TRUE)
