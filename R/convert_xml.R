a20cc85d11f6a794f3162447f193c763331eb8f6
Gustavo Guimarães <49100132+gustavo234guimaraes@users.noreply.github.com>
2024-04-03 09:34:16 -0300

    Add files via upload

--- R/convert_xml.R
+++ R/convert_xml.R
@@ -1,12 +1,12 @@
 convert_xml<-function(file,ext='.csv',out_dir=""){
   
   arqXML<-xml2::read_xml(file)
-  node<-xml_name(xml_child(arqXML))
+  node<-xml2::xml_name(xml_child(arqXML))
   
-  nodes<-xml_find_all(arqXML, paste0("//", node))
+  nodes<-xml2::xml_find_all(arqXML, paste0("//", node))
   lapply(nodes, function(x){
-    parent <- data.frame(as.list(xml_attrs(x)), stringsAsFactors = FALSE)
-    kids <- bind_rows(lapply(xml_children(x), get_children))
+    parent <- data.frame(as.list(xml2::xml_attrs(x)), stringsAsFactors = FALSE)
+    kids <- bind_rows(lapply(xml2::xml_children(x), get_children))
     cbind.data.frame(parent, kids, stringsAsFactors = FALSE)
   }) %>% unlist(recursive = F) %>%
     suppressMessages() %>% 
--- R/read_xml.R
+++ R/read_xml.R
@@ -4,14 +4,14 @@ read_xml<-function(file,node=NULL){
   
   arqXML<-xml2::read_xml(file)
   if(is.null(node)){
-    node<-xml_name(xml_child(arqXML))
+    node<-xml2::xml_name(xml_child(arqXML))
   }
   
   
-  nodes<-xml_find_all(arqXML, paste0("//", node))
+  nodes<-xml2::xml_find_all(arqXML, paste0("//", node))
   lapply(nodes, function(x){
-    parent <- data.frame(as.list(xml_attrs(x)), stringsAsFactors = FALSE)
-    kids <- bind_rows(lapply(xml_children(x), get_children))
+    parent <- data.frame(as.list(xml2::xml_attrs(x)), stringsAsFactors = FALSE)
+    kids <- bind_rows(lapply(xml2::xml_children(x), get_children))
     cbind.data.frame(parent, kids, stringsAsFactors = FALSE)
   }) %>% unlist(recursive = F) %>%
     suppressMessages() %>% 
